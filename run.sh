e63dd13e2027e816abcf073ea1e0fc952e895267
Srishti Sharma <163334619+Srishttech@users.noreply.github.com>
2026-06-25 21:23:12 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -52,4 +52,4 @@ fi
 # Launch Streamlit
 echo
 echo "[+] Starting Application..."
-streamlit run app.py
+streamlit run app.py --server.address=0.0.0.0 --server.port=8501 --server.enableCORS=false --server.enableXsrfProtection=false
